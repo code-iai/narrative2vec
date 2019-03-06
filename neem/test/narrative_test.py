73baca9e51940432a79f86012141c7d547ae813b
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-03-06 12:52:28 +0100

    test(narrative.py): Adds test for convert to csv file

--- neem/test/narrative_test.py
+++ neem/test/narrative_test.py
@@ -1,10 +1,26 @@
-from os.path import join, abspath, dirname
+import shutil
+from os.path import join, abspath, dirname, exists
+from os import listdir, makedirs
 from neem.narrative import Narrative
 
 ABSOULTE_PATH_TO_NARRATIVE_TEST_PARENT_DIR = dirname(abspath(__file__))
 NEEM_TEST_FILE_PATH = join(ABSOULTE_PATH_TO_NARRATIVE_TEST_PARENT_DIR, 'test_files', '1551794276507338.owl')
+DESTINATION_PATH_DIR = join(ABSOULTE_PATH_TO_NARRATIVE_TEST_PARENT_DIR, 'test_result')
 
 
 def test_should_read_neem_narrative_file():
     Narrative(NEEM_TEST_FILE_PATH)
     assert True
+
+
+def test_should_convert_narrative_to_csv_file():
+    if exists(DESTINATION_PATH_DIR):
+        shutil.rmtree(DESTINATION_PATH_DIR)
+
+    makedirs(DESTINATION_PATH_DIR)
+
+    narrative = Narrative(NEEM_TEST_FILE_PATH)
+    narrative.transform_to_csv_file(DESTINATION_PATH_DIR)
+    test_result_files = listdir(DESTINATION_PATH_DIR)
+
+    assert len(test_result_files) == 3
