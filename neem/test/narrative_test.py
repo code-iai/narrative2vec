389b77e329370a198630bfda1b8665aea4c4c324
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-03-06 11:16:49 +0100

    test(Narrative.py): Adds absolute path to the narrative test to avoid IO exceptions

--- neem/test/narrative_test.py
+++ neem/test/narrative_test.py
@@ -1,7 +1,8 @@
-from os.path import join
+from os.path import join, abspath, dirname
 from neem.narrative import Narrative
 
-NEEM_TEST_FILE_PATH = join('test_files', '1551794276507338.owl')
+ABSOULTE_PATH_TO_NARRATIVE_TEST_PARENT_DIR = dirname(abspath(__file__))
+NEEM_TEST_FILE_PATH = join(ABSOULTE_PATH_TO_NARRATIVE_TEST_PARENT_DIR, 'test_files', '1551794276507338.owl')
 
 
 def test_should_read_neem_narrative_file():
