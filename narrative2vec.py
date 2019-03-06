e233e81206e02c02943d1cbf70791728dffa34a3
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-03-06 13:21:13 +0100

    refactor(narrative2vec.py): Updates main module to csv refactor

--- narrative2vec.py
+++ narrative2vec.py
@@ -1,62 +1,8 @@
-import csv
 import sys
-from os import listdir, makedirs
-from os.path import isdir, join, exists, basename
-from neem.narrative import Narrative, get_vector_definition, get_reasoning_task_vector_definition, \
-    get_poses_vector_definition
-import shutil
 
-
-def write_reasoning_task_to_csv(vecs, result_dir_path):
-    narrative_path = join(result_dir_path, 'reasoning_tasks.csv')
-
-    with open(narrative_path, 'wb') as csvfile:
-        vec_writer = csv.writer(csvfile, delimiter=';')
-        vec_writer.writerow(get_reasoning_task_vector_definition())
-
-        for vec in vecs:
-            vec_writer.writerow(vec)
-
-
-def write_narrative_vector_to_csv(vecs, result_dir_path):
-    narrative_path = join(result_dir_path, 'narrative.csv')
-
-    with open(narrative_path, 'wb') as csvfile:
-        vec_writer = csv.writer(csvfile, delimiter=';')
-        vec_writer.writerow(get_vector_definition())
-
-        for vec in vecs:
-            vec_writer.writerow(vec)
-
-
-def write_poses_to_csv(vecs, result_dir_path):
-    narrative_path = join(result_dir_path, 'poses.csv')
-
-    with open(narrative_path, 'wb') as csvfile:
-        vec_writer = csv.writer(csvfile, delimiter=';')
-        vec_writer.writerow(get_poses_vector_definition())
-
-        for vec in vecs:
-            vec_writer.writerow(vec)
-
-
-def transform_neem_to_csv(neem_path, result_dir_path):
-    experiment_name = basename(neem_path).split('.')[0]
-    narrative = Narrative(neem_path)
-
-    store_path = join(result_dir_path, experiment_name)
-
-    if exists(store_path):
-        shutil.rmtree(store_path)
-
-    makedirs(store_path)
-
-    vecs = narrative.toVecs()
-
-    write_narrative_vector_to_csv(vecs, store_path)
-    write_reasoning_task_to_csv(narrative.get_reasoning_tasks(), store_path)
-    write_poses_to_csv(narrative.get_poses(), store_path)
-
+from os import listdir
+from os.path import isdir, join
+from neem.narrative import Narrative
 
 if __name__ == "__main__":
     args = sys.argv[1:]
@@ -66,6 +12,9 @@ if __name__ == "__main__":
     if isdir(path):
         for neemName in listdir(path):
             neem_path = join(path, neemName)
-            transform_neem_to_csv(neem_path, result_dir_path)
+
+            narrative = Narrative(neem_path)
+            narrative.transform_to_csv_file(result_dir_path)
     else:
-        transform_neem_to_csv(path, result_dir_path)
+        narrative = Narrative(path)
+        narrative.transform_to_csv_file(result_dir_path)
