fd91815d17d41419044b6f8323f320badbbf4143
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-03-13 13:25:52 +0100

    feat(setup.py): Adds setup file

---
+++ setup.py
@@ -0,0 +1,38 @@
+# Copyright 2018 Institute for Artificial Intelligence - University of Bremen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="narrative2vec",
+    version="1.0.0",
+    author="Sebastian Koralewski",
+    author_email="seba@cs.uni-bremen.de",
+    description="Transforms NEEM narrative data into CSV files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/code-iai/narrative2neem",
+    packages=setuptools.find_packages(exclude=("test",)),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
